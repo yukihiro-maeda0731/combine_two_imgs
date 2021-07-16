6de358dfb9c5a08a9bb2d2f974bd85936234db67
前田 <sloth19920731@gmail.com>
2021-07-16 23:37:17 +0900

    Stack widgetで合成対象の画像2つ表示

---
+++ android/app/src/main/kotlin/com/example/combine_two_imgs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.combine_two_imgs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  _combineTwoImgs(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.network("https://picsum.photos/250?image=9"),
+          ),
+          Center(
+            child: FlutterLogo()
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _combineTwoImgs(),
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
